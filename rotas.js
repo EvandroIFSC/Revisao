2dfbe40c3c5e57da3178bf772ea28e5c1a03b8bf
EvandroIFSC <evandro.f05@aluno.ifsc.edu.br>
2023-04-10 20:38:33 -0300

    Criação dos arquivos Servidor.js e Rotas.js

---
+++ rotas.js
@@ -0,0 +1,14 @@
+const express = require ("express")
+const routes = express.Router()
+
+module.exports = routes
+
+
+
+
+
+
+
+
+
+
---
+++ servidor.js
@@ -0,0 +1,12 @@
+const express =require ("express")
+
+const server =express()
+
+server .use(express.json())
+server .use(express.urlencoded({extended:true}))
+server.listen(3000,()=>{
+    console.log("Servidpr em execução...")
+})
+
+
+
